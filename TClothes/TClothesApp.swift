4c29fa3e5472cf17936956fd813a820887d0fd27
tharmJaa <timeruchu0808@gmail.com>
2023-03-28 13:27:02 +0700

    Initial Commit

---
+++ TClothes/TClothesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TClothesApp.swift
+//  TClothes
+//
+//  Created by Time Ruchutrakool on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TClothesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
